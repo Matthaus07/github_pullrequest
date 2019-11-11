509b4c187b0afdea5f26c1789c72a8f70a7c81b3
Matthaus07 <mathaus_soares07@hotmail.com>
2019-11-11 16:22:49 -0300

    using material-ui

--- src/client/src/component/Table.js
+++ src/client/src/component/Table.js
@@ -1,40 +1,82 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import {
+    Table, TableBody, TableCell, TableHead, TableRow, Paper, Container, CssBaseline
+    , Grid, Box, Typography
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(theme => ({
+    wrapper: {
+        padding: '2em'
+    },
+    rootTabela: {
+        width: '100%',
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 650,
+    },
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%'
+    }
+}));
 
-export default function UserTable({rows}) {
+
+export default function UserTable({ rows }) {
+    const classes = useStyles();
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>User Name</th>
-                    <th>Title</th>
-                    <th>Url Pull</th>
-                    <th>State</th>
-                    <th>Create At</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                   rows.length > 0 ? (
-                    rows.map((row, i) => (
+        <Fragment>
+            <CssBaseline />
+            <Grid container spacing={12}>
 
-                        <tr key={row.id}>
-                            <td>{row.id}</td>
-                            <td>{row.username}</td>
-                            <td>{row.title}</td>
-                            <td>{row.urlpull}</td>
-                            <td>{row.state}</td>
-                            <td>{row.createatpull}</td>
-                        </tr>
-                    ))
-                ) : (
-                        <tr>
-                            <td colSpan={3}>{rows[0]} No Users</td>
-                        </tr>
-                    )
-                }
-            </tbody>
-        </table>
+                <Grid item xs={12}>
+                    <Paper className={classes.rootTabela}>
+                        <Table className={classes.table} size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>#</TableCell>
+                                    <TableCell>User</TableCell>
+                                    <TableCell>Title</TableCell>
+                                    <TableCell>Created At</TableCell>
+                                    <TableCell>State</TableCell>
+                                    <TableCell>url Pull</TableCell>
+
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.length > 0 ? (
+                                    rows.map((row, i) => (
+                                        <TableRow data-space="home" id="forn"
+
+                                            key={row.id}>
+                                            <TableCell>{i + 1}</TableCell>
+                                            <TableCell component="th" scope="row">
+                                                {row.username}
+                                            </TableCell>
+                                            <TableCell component="th" scope="row">
+                                                {row.title}
+                                            </TableCell>
+                                            <TableCell>{row.createatpull}</TableCell>
+                                            <TableCell>{row.state}</TableCell>
+                                            <TableCell>{row.urlpull}</TableCell>
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                        <tr>
+                                            <td colSpan={3}>{rows[0]} No Users</td>
+                                        </tr>
+                                    )
+                                }
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Fragment>
+
     )
 
 }
